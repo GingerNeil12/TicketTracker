d0db93318643fe3f72d191d27ed41538d036ec62
Luke <l.norman.dev@gmail.com>
2020-09-25 18:24:36 +0100

    Added auditable entity

---
+++ TicketTracker/TicketTracker.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicketTracker.Domain.Common
+{
+    public abstract class AuditableEntity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
