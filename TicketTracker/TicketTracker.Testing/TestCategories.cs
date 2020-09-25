e2925a44a8476b0f0109f04b314e676a800fbfb8
Neil Earlam <neil.earlam2019@outlook.com>
2020-09-25 18:59:54 +0100

    Added the Testing base project

---
+++ TicketTracker/TicketTracker.Testing/TestCategories.cs
@@ -0,0 +1,8 @@
+﻿namespace TicketTracker.Testing
+{
+    public static class TestCategories
+    {
+        public const string Unit = "Unit";
+        public const string Integration = "Integration";
+    }
+}
