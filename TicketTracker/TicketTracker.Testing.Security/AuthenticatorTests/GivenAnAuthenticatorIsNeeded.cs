777dc86f8ddfe5bcd47bcee31684ccdecb231f8b
Neil Earlam <neil.earlam2019@outlook.com>
2020-09-27 11:45:57 +0100

    Need to fix the UserManager mocking

--- TicketTracker/TicketTracker.Testing.Security/AuthenticatorTests/GivenAnAuthenticatorIsNeeded.cs
+++ TicketTracker/TicketTracker.Testing.Security/AuthenticatorTests/GivenAnAuthenticatorIsNeeded.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TicketTracker.Application.Interfaces.Security;
+using TicketTracker.Infrastructure.Security.Interfaces;
+using TicketTracker.Infrastructure.Security.Models;
+using TicketTracker.Infrastructure.Security.Services;
+using TicketTracker.Testing.Security.Common;
 
 namespace TicketTracker.Testing.Security.AuthenticatorTests
 {
     [TestClass]
-    public class GivenAnAuthenticatorIsNeeded
+    public class GivenAnAuthenticatorIsNeeded : AuthenticationBaseTest
     {
+        [TestMethod]
+        public void WhenConstructed_WithValidArguments_ConstructsProperly()
+        {
+            // Arrange
+            var userManager = CreateMockUserManager();
+            var existingUser = new Mock<IExistingUser>(MockBehavior.Strict).Object;
+            var tokenGenerator = new Mock<ITokenGenerator>(MockBehavior.Strict).Object;
+
+            // Act
+            var result = new Authenticator(userManager, existingUser, tokenGenerator);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IAuthenticator));
+        }
     }
 }
---
+++ TicketTracker/TicketTracker.Testing.Security/Common/AuthenticationBaseTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using TicketTracker.Infrastructure.Security.Models;
+
+namespace TicketTracker.Testing.Security.Common
+{
+    public abstract class AuthenticationBaseTest
+    {
+        // TODO: Clean this up.
+        protected UserManager<ApplicationUser> CreateMockUserManager()
+        {
+            var userStore = new Mock<IUserStore<ApplicationUser>>(MockBehavior.Strict).Object;
+            var options = new Mock<IOptions<IdentityOptions>>(MockBehavior.Strict).Object;
+            var passwordHasher = new Mock<IPasswordHasher<ApplicationUser>>(MockBehavior.Strict).Object;
+            var userValidators = new Mock<IEnumerable<IUserValidator<ApplicationUser>>>(MockBehavior.Strict).Object;
+            var passwordValidators = new Mock<IEnumerable<IPasswordValidator<ApplicationUser>>>(MockBehavior.Strict).Object;
+            var errors = new Mock<IdentityErrorDescriber>(MockBehavior.Strict).Object;
+            var keyNormalizer = new Mock<ILookupNormalizer>(MockBehavior.Strict).Object;
+            var services = new Mock<IServiceProvider>(MockBehavior.Strict).Object;
+            var logger = new Mock<ILogger<UserManager<ApplicationUser>>>(MockBehavior.Strict).Object;
+            return new UserManager<ApplicationUser>
+            (
+                userStore,
+                options,
+                passwordHasher,
+                userValidators,
+                passwordValidators,
+                keyNormalizer,
+                errors,
+                services,
+                logger
+            );
+        }
+    }
+}
