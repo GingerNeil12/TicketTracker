a2dfae078a85fee771391173f5ccc2517f596be0
Neil Earlam <neil.earlam2019@outlook.com>
2020-10-05 00:43:03 +0100

    Added default CurrentDateTime provider for system

---
+++ TicketTracker/TicketTracker.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TicketTracker.Application.Interfaces.Common;
+using TicketTracker.Infrastructure.Services;
+
+namespace TicketTracker.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<ICurrentDateTime, CurrentDateTime>();
+        }
+    }
+}
---
+++ TicketTracker/TicketTracker.Infrastructure/Services/CurrentDateTime.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using TicketTracker.Application.Interfaces.Common;
+
+namespace TicketTracker.Infrastructure.Services
+{
+    public class CurrentDateTime : ICurrentDateTime
+    {
+        public DateTime Now => DateTime.Now;
+    }
+}
