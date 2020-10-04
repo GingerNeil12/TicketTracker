34ca3a39d70856d5c6562afaaf5cce97b34d1f60
Neil Earlam <neil.earlam2019@outlook.com>
2020-10-04 17:24:30 +0100

    Tests for building the TokenGenerator completed and passing

--- TicketTracker/TicketTracker.Testing.Security/TokenGeneratorTests/GivenAtokenGeneratorIsNeeded.cs
+++ TicketTracker/TicketTracker.Testing.Security/TokenGeneratorTests/GivenAtokenGeneratorIsNeeded.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketTracker.Infrastructure.Security.Interfaces;
+using TicketTracker.Infrastructure.Security.Services;
 using TicketTracker.Testing.Security.Common;
 
 namespace TicketTracker.Testing.Security.TokenGeneratorTests
@@ -10,7 +14,30 @@ namespace TicketTracker.Testing.Security.TokenGeneratorTests
         [TestMethod]
         public void WhenConstructed_WithValidArguments_ConstructsProperly()
         {
-            var config = GetConfiguration();
+            // Arrange
+            var configuration = GetConfiguration();
+
+            // Act
+            var result = new TokenGenerator(configuration);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ITokenGenerator));
+        }
+
+        [TestMethod]
+        public void WhenConstructed_WithNullArguments_ThrowsNullException()
+        {
+            // Arrange
+            IConfiguration configuration = null;
+
+            // Act
+            Action test = () =>
+                new TokenGenerator(configuration);
+
+            // Assert
+            var exception = Assert.ThrowsException<ArgumentNullException>(test);
+            Assert.AreEqual(nameof(configuration), exception.ParamName);
         }
     }
 }
