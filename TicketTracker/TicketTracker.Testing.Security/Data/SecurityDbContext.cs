f97f2388933eb5d4f6b2d99ddfb3ffaa25a4019d
Neil Earlam <neil.earlam2019@outlook.com>
2020-10-04 23:56:56 +0100

    Added A dbcontext to help with testing in the security project

---
+++ TicketTracker/TicketTracker.Testing.Security/Data/SecurityDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using TicketTracker.Infrastructure.Security.Models;
+
+namespace TicketTracker.Testing.Security.Data
+{
+    internal class SecurityDbContext
+        : IdentityDbContext<ApplicationUser>
+    {
+    }
+}
